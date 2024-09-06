9d7412c569e6f87ab8fc3400c425b8ddd84e4683
IsmaelSidney <mael-sf@hotmail.com>
2024-09-06 08:29:09 -0300

    Solução do desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,24 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+
+    public class Nokia : Smartphone 
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+    // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+           Console.WriteLine($"O aplicativo: {nomeApp} foi instalado no Nokia {Modelo} pela PlayStore" +
+                    $" do numero: {Numero} IMEI:{IMEI} e com {Memoria}GB de memoria. "); 
+        }
     }
+        
+       
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Instanciando Nokia");
+Smartphone nokia = new Nokia(numero: "1234567",modelo: "Nokia2030",imei: "123wer123", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Maps");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Instanciando Iphone");
+Smartphone iphone = new Iphone(numero: "1234567",modelo: "Iphone 1",imei: "123wer123", memoria: 164);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Google Maps");
